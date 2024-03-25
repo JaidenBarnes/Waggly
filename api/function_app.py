2b390ffee47278805c9ed75ac999aa3a8bf89d88
JBarnes <98770634+JaidenBarnes@users.noreply.github.com>
2024-03-25 15:27:11 +0000

    15:27

--- api/function_app.py
+++ api/function_app.py
@@ -5,9 +5,9 @@ app = func.FunctionApp()
 
 @app.function_name(name="postregisterwalker")
 @app.route(route="postregisterwalker", auth_level=func.AuthLevel.ANONYMOUS)
-@app.cosmos_db_output(arg_name="dogWalkerDocument", database_name="waggly-database", container_name="waggly_container", connection="CosmosDbConnectionSetting")
+@app.cosmos_db_output(arg_name="WalkerDocument", database_name="waggly-database", container_name="waggly_container", connection="CosmosDbConnectionSetting")
 
-def createWalker(req: func.HttpRequest, dogWalkerDocument: func.Out[func.Document]) -> func.HttpResponse:
+def createWalker(req: func.HttpRequest, WalkerDocument: func.Out[func.Document]) -> func.HttpResponse:
     walker_id = str(uuid.uuid4())
     walker_firstname = req.params.get('walkerfirstname')
     walker_lastname = req.params.get('walkerlastname')
@@ -16,7 +16,7 @@ def createWalker(req: func.HttpRequest, dogWalkerDocument: func.Out[func.Documen
     walker_email = req.params.get('walkeremail')
 
     if walker_firstname and walker_lastname and walker_location and walker_phonenumber and walker_email:
-        dogWalkerDocument.set(func.Document.from_dict({"id": walker_id, "walker_firstname": walker_firstname, "walker_lastname": walker_lastname, "walker_location": walker_location, "walker_phonenumber": walker_phonenumber, "walker_email": walker_email}))
+        WalkerDocument.set(func.Document.from_dict({"id": walker_id, "walker_firstname": walker_firstname, "walker_lastname": walker_lastname, "walker_location": walker_location, "walker_phonenumber": walker_phonenumber, "walker_email": walker_email}))
         return func.HttpResponse(f"stored details: {walker_id}\n{walker_firstname}\n{walker_lastname}\n{walker_location}\n{walker_phonenumber}\n{walker_email}\n")
     else:
         return func.HttpResponse("unable to run function you melt", status_code = 200)
